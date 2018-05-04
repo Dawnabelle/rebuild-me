328bd3f5bc71dc9426fb3d11b1578243a0bd8a5e
Dawnabelle <dawn.mott@gmail.com>
2018-05-04 14:27:00 -0700

    create nav-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BeyImagesComponent } from './bey-images/bey-images.component';
+import { MenuComponent } from './menu/menu.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BeyImagesComponent
+    BeyImagesComponent,
+    MenuComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule
