a67e02ca4eb6f7d7b249abbb6fc0a9225ea4bec5
Dawnabelle <dawn.mott@gmail.com>
2018-05-06 14:15:31 -0700

    add jquery and selector menu

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-nav-menu',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.initHandleMainMenu();
+  }
+
+  initHandleMainMenu() {
+    $(document).ready(function() {
+
+      // Add toggle menu open class
+      $('.container-nav-menu .hamburger').click(function(e) {
+        e.preventDefault();
+        let menu = $('.container-nav-menu');
+        menu.toggleClass('nav-menu-open');
+      });
+
+    });
+  }
 
   ngOnInit() {
   }
